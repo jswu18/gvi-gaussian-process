bf887af57db91598bae9a933420436442abeecb0
James Wu <jswu18@gmail.com>
2023-03-06 18:51:24 +0000

    add comments for kernel and mean functions

--- src/mean_functions/mean_functions.py
+++ src/mean_functions/mean_functions.py
@@ -9,4 +9,15 @@ from src.module import Module
 class MeanFunction(Module, ABC):
     @abstractmethod
     def predict(self, parameters: FrozenDict, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        Computes the mean function at the given points.
+            - n is the number of points in x
+            - d is the number of dimensions
+        Args:
+            parameters: parameters of the mean function
+            x: design matrix of shape (n, d)
+
+        Returns: the mean function evaluated at the points in x of shape (n, 1)
+
+        """
         pass
