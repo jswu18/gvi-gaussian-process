b083550678738030e098e7f1a533071242ca52cd
James Wu <jswu18@gmail.com>
2023-03-06 14:49:35 +0000

    add back warning msg temporarily
    Former-commit-id: 686369d531a444f9bb669ec4b907ac2c2a37a45e

--- src/utils.py
+++ src/utils.py
@@ -19,7 +19,10 @@ def compute_covariance_eigenvalues(
     matrix: jnp.ndarray, logging_warning_threshold: float = 5e-2
 ) -> jnp.ndarray:
     covariance_eigenvalues, _ = jnp.linalg.eigh(matrix)
-    # minimum_covariance_eigenvalue = jnp.min(covariance_eigenvalues)
+    minimum_covariance_eigenvalue = jnp.min(covariance_eigenvalues)
+    spectrum_ratio = jnp.abs(minimum_covariance_eigenvalue) / jnp.max(
+        covariance_eigenvalues
+    )
     # if minimum_covariance_eigenvalue < 0:
     #     spectrum_ratio = jnp.abs(minimum_covariance_eigenvalue) / jnp.max(
     #         covariance_eigenvalues
@@ -28,6 +31,10 @@ def compute_covariance_eigenvalues(
     #         logging.warning(
     #             f"Covariance has negatives. {spectrum_ratio=}, {logging_warning_threshold=}, {covariance_eigenvalues=}"
     #         )
+
+    logging.warning(
+        f"Covariance {spectrum_ratio=}, {jnp.min(covariance_eigenvalues)=}, {jnp.max(covariance_eigenvalues)=}"
+    )
 
     # following
     # https://github.com/google/neural-tangents/blob/5d38d3e97a2e251c37bb1ba44a89cbb8565a5459/neural_tangents/_src/predict.py#L1266
