a43b117b1fab5fa6608f71d1eaed6a94a4b311cf
James Wu <jswu18@gmail.com>
2023-06-05 16:29:03 +0100

    add tests for approx kernels
    Former-commit-id: 60ecbe7e2bcd1bef065d759844ceed3165667bf5

---
+++ tests/mockers/reference_kernel.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict, Union
+
+import jax.numpy as jnp
+import pydantic
+from flax.core.frozen_dict import FrozenDict
+
+from src.kernels.kernels import Kernel
+from src.parameters.kernels.kernels import KernelParameters
+from src.parameters.module import ModuleParameters
+
+PRNGKey = Any  # pylint: disable=invalid-name
+
+
+class ReferenceKernelParametersMock(KernelParameters):
+    pass
+
+
+class ReferenceKernelMock(Kernel):
+    sigma_matrix = jnp.ones((5, 5))
+
+    @pydantic.validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def generate_parameters(
+        self, parameters: Union[Dict, FrozenDict]
+    ) -> ModuleParameters:
+        pass
+
+    @pydantic.validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def initialise_random_parameters(
+        self,
+        key: PRNGKey,
+    ) -> ModuleParameters:
+        pass
+
+    def _calculate_gram(
+        self,
+        x: jnp.ndarray,
+        y: jnp.ndarray = None,
+        parameters: ReferenceKernelParametersMock = None,
+    ) -> jnp.ndarray:
+        return jnp.ones((x.shape[0], y.shape[0]))
+
+
+def calculate_reference_gram_mock(
+    x: jnp.ndarray,
+    y: jnp.ndarray = None,
+) -> jnp.ndarray:
+    return jnp.ones((x.shape[0], y.shape[0]))
