07358fc9cc12a49d499bfc18b218f609eb62911c
James Wu <jswu18@gmail.com>
2023-06-22 12:49:27 +0100

    add eigval computation for product of symmetric matrices

--- src/utils/matrix_operations.py
+++ src/utils/matrix_operations.py
@@ -1,6 +1,7 @@
 import logging
 
 import jax.numpy as jnp
+import jax.scipy as jsp
 from jax.experimental import host_callback
 
 
@@ -49,6 +50,7 @@ def compute_covariance_eigenvalues(
     """
     Compute the eigenvalues of a covariance matrix. Because the matrix is a covariance matrix, we expect
      the eigenvalues to be positive. Thus, for numerical stability the eigenvalues are clipped to be non-negative.
+     This implementation is not available for gpus.
     Follows from:
     https://github.com/google/neural-tangents/blob/5d38d3e97a2e251c37bb1ba44a89cbb8565a5459/neural_tangents/_src/predict.py#L1266
     Args:
@@ -59,7 +61,6 @@ def compute_covariance_eigenvalues(
 
     """
     covariance_eigenvalues = jnp.linalg.eigvals(matrix)  # doesn't work for GPU
-    # covariance_eigenvalues, _ = jnp.linalg.eigh(matrix) # works for GPU but requires symmetric matrix
     minimum_covariance_eigenvalue = jnp.min(covariance_eigenvalues)
     spectrum_ratio = jnp.abs(minimum_covariance_eigenvalue) / jnp.max(
         covariance_eigenvalues
@@ -68,6 +69,40 @@ def compute_covariance_eigenvalues(
     #     _eigenvalue_warning,
     #     (minimum_covariance_eigenvalue, spectrum_ratio, logging_warning_threshold, covariance_eigenvalues),
     # )
+    return jnp.clip(
+        covariance_eigenvalues,
+        a_min=0,
+        a_max=None,
+    ).real
+
+
+def compute_product_eigenvalues(
+    matrix_a: jnp.ndarray,
+    matrix_b: jnp.ndarray,
+):
+    """
+    Computes the eigenvalues of a product of two symmetric matrices:
+        eig(A*B))
+    We will use the fact that:
+        eig(A*B) = eig(sqrt(B)*A*sqrt(B))
+    where A and B are both symmetric matrices.
+    This implementation uses jnp.linalg.eigh which has GPU implementation
+    Args:
+        matrix_a: symmetric matrix of shape (n, n)
+        matrix_b: symmetric matrix of shape (n, n)
+
+    Returns: the eigenvalues of the product matrix_a*matrix_b
+
+    """
+    matrix_a_eigenvalues, _ = jnp.linalg.eigh(matrix_a)
+    matrix_b_sqrt = jsp.linalg.sqrtm(matrix_b)
+    matrix_b_sqrt_eigenvalues, _ = jnp.linalg.eigh(matrix_b_sqrt)
+    covariance_eigenvalues = jnp.prod(
+        jnp.stack(
+            (matrix_b_sqrt_eigenvalues, matrix_a_eigenvalues, matrix_b_sqrt_eigenvalues)
+        ),
+        axis=0,
+    )
     return jnp.clip(
         covariance_eigenvalues,
         a_min=0,
