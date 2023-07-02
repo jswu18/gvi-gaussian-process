fc90875ab7ba738da55a2c16fc6e78d5b883674f
James Wu <jswu18@gmail.com>
2023-07-02 15:59:50 +0100

    add gaussian measures in abc distinct means classifier
    Former-commit-id: c5a549cae203415afb7d13ec6c038f5ab89c5aff

--- src/parameters/classification_models/distinct_means.py
+++ src/parameters/classification_models/distinct_means.py
@@ -7,13 +7,14 @@ from src.parameters.classification_models.classification_models import (
 from src.parameters.gaussian_measures.approximate_gaussian_measures import (
     ApproximateGaussianMeasureParameters,
 )
+from src.parameters.gaussian_measures.gaussian_measures import GaussianMeasureParameters
 from src.parameters.gaussian_measures.reference_gaussian_measures import (
     ReferenceGaussianMeasureParameters,
 )
 
 
 class DistinctMeansClassificationModelParameters(ClassificationModelParameters, ABC):
-    pass
+    gaussian_measures: Dict[Any, GaussianMeasureParameters]
 
 
 class ReferenceDistinctMeansClassificationModelParameters(
