1b543ad82438aa37231c2efbd4c7a6ea76cb6e88
James Wu <jswu18@gmail.com>
2023-07-02 15:57:46 +0100

    add checkpoint distinguishing distinct mean classification models

--- src/parameters/classification_models/distinct_means.py
+++ src/parameters/classification_models/distinct_means.py
@@ -1,23 +1,28 @@
 from abc import ABC
 from typing import Any, Dict
 
+from src.parameters.classification_models.classification_models import (
+    ClassificationModelParameters,
+)
 from src.parameters.gaussian_measures.approximate_gaussian_measures import (
     ApproximateGaussianMeasureParameters,
 )
-from src.parameters.gaussian_measures.gaussian_measures import GaussianMeasureParameters
 from src.parameters.gaussian_measures.reference_gaussian_measures import (
     ReferenceGaussianMeasureParameters,
 )
-from src.parameters.module import ModuleParameters
 
 
-class ClassificationModelParameters(ModuleParameters, ABC):
-    gaussian_measures: Dict[Any, GaussianMeasureParameters]
+class DistinctMeansClassificationModelParameters(ClassificationModelParameters, ABC):
+    pass
 
 
-class ReferenceClassificationModelParameters(ClassificationModelParameters):
+class ReferenceDistinctMeansClassificationModelParameters(
+    DistinctMeansClassificationModelParameters
+):
     gaussian_measures: Dict[Any, ReferenceGaussianMeasureParameters]
 
 
-class ApproximateClassificationModelParameters(ClassificationModelParameters):
+class ApproximateDistinctMeansClassificationModelParameters(
+    DistinctMeansClassificationModelParameters
+):
     gaussian_measures: Dict[Any, ApproximateGaussianMeasureParameters]
