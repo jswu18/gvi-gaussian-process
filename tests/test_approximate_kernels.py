79c41ea3c789463f4d9136a7c2a9e37428947bc1
James Wu <jswu18@gmail.com>
2023-07-15 13:15:35 +0100

    update sigma paramterisation with lower triangle and log diagonal paramters

--- tests/test_approximate_kernels.py
+++ tests/test_approximate_kernels.py
@@ -16,7 +16,7 @@ config.update("jax_enable_x64", True)
 
 
 @pytest.mark.parametrize(
-    "x_train,x_inducing,log_el_matrix",
+    "x_train,x_inducing,target_el_matrix_lower_triangle",
     [
         [
             jnp.array(
@@ -34,17 +34,17 @@ config.update("jax_enable_x64", True)
             ),
             jnp.array(
                 [
-                    [-0.34657359027997275, -23.025850929940457],
-                    [-23.025850929940457, -0.34657359027997275],
+                    [0, 0],
+                    [0, 0],
                 ]
             ),
         ],
     ],
 )
-def test_svgp_sigma_matrix(
+def test_svgp_sigma_lower_triangle_matrix(
     x_train: jnp.ndarray,
     x_inducing: jnp.ndarray,
-    log_el_matrix: jnp.ndarray,
+    target_el_matrix_lower_triangle: jnp.ndarray,
 ):
     approximate_kernel = StochasticVariationalGaussianProcessKernel(
         reference_kernel_parameters=ReferenceKernelParametersMock(),
@@ -53,14 +53,57 @@ def test_svgp_sigma_matrix(
         inducing_points=x_inducing,
         training_points=x_train,
     )
+    el_matrix_lower_triangle, _ = approximate_kernel.initialise_el_matrix_parameters()
     assert jnp.array_equal(
-        approximate_kernel.initialise_log_el_matrix(),
-        log_el_matrix,
+        el_matrix_lower_triangle,
+        target_el_matrix_lower_triangle,
     )
 
 
 @pytest.mark.parametrize(
-    "x_train,x_inducing,log_el_matrix,gram",
+    "x_train,x_inducing,target_el_matrix_log_diagonal",
+    [
+        [
+            jnp.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [5.0, 1.0, 9.0],
+                    [1.5, 2.5, 3.5],
+                ]
+            ),
+            jnp.array(
+                [
+                    [5.0, 1.0, 9.0],
+                    [1.5, 2.5, 3.5],
+                ]
+            ),
+            jnp.array(
+                [-0.34657359027997275, -0.34657359027997275],
+            ),
+        ],
+    ],
+)
+def test_svgp_sigma_diagonal_matrix(
+    x_train: jnp.ndarray,
+    x_inducing: jnp.ndarray,
+    target_el_matrix_log_diagonal: jnp.ndarray,
+):
+    approximate_kernel = StochasticVariationalGaussianProcessKernel(
+        reference_kernel_parameters=ReferenceKernelParametersMock(),
+        log_observation_noise=jnp.log(1),
+        reference_kernel=ReferenceKernelMock(calculate_reference_gram_eye_mock),
+        inducing_points=x_inducing,
+        training_points=x_train,
+    )
+    _, el_matrix_log_diagonal = approximate_kernel.initialise_el_matrix_parameters()
+    assert jnp.array_equal(
+        el_matrix_log_diagonal,
+        target_el_matrix_log_diagonal,
+    )
+
+
+@pytest.mark.parametrize(
+    "x_train,x_inducing,el_matrix_lower_triangle,el_matrix_log_diagonal,gram",
     [
         [
             jnp.array(
@@ -78,18 +121,22 @@ def test_svgp_sigma_matrix(
             ),
             jnp.array(
                 [
-                    [5.0, 0.1],
-                    [0.2, 2.5],
+                    [0, 0],
+                    [0.2, 0],
                 ]
             ),
-            22535.395464414618 * jnp.ones((3, 3)),
+            jnp.array(
+                [4.3, 2.2],
+            ),
+            5516.760470427696 * jnp.ones((3, 3)),
         ],
     ],
 )
 def test_svgp_kernel_grams(
     x_train: jnp.ndarray,
     x_inducing: jnp.ndarray,
-    log_el_matrix: jnp.ndarray,
+    el_matrix_lower_triangle: jnp.ndarray,
+    el_matrix_log_diagonal: jnp.ndarray,
     gram: float,
 ):
     approximate_kernel = StochasticVariationalGaussianProcessKernel(
@@ -102,7 +149,8 @@ def test_svgp_kernel_grams(
     assert jnp.array_equal(
         approximate_kernel.calculate_gram(
             StochasticVariationalGaussianProcessKernelParameters(
-                log_el_matrix=log_el_matrix,
+                el_matrix_lower_triangle=el_matrix_lower_triangle,
+                el_matrix_log_diagonal=el_matrix_log_diagonal,
             ),
             x=x_train,
         ),
