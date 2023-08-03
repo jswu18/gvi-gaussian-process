48191324e8288bdb336a325e07c18883ec2224a2
James Wu <jswu18@gmail.com>
2023-08-03 17:15:00 +0100

    remove unused method
    Former-commit-id: 6566ab22d633a39a08c7ce9cac0b8800e61aac37

--- src/distributions.py
+++ src/distributions.py
@@ -1,8 +1,6 @@
 from abc import ABC
 from typing import Literal
 
-import jax.numpy as jnp
-
 from src.module import ModuleParameters
 from src.utils.custom_types import JaxArrayType
 
@@ -15,14 +13,6 @@ class Gaussian(Distribution):
     mean: JaxArrayType[Literal["float64"]]
     covariance: JaxArrayType[Literal["float64"]]
     full_covariance: bool = True
-
-    @staticmethod
-    def calculate_log_likelihood(
-        mean: jnp.ndarray,
-        covariance: jnp.ndarray,
-        y: jnp.ndarray,
-    ) -> jnp.float64:
-        return
 
 
 class Multinomial(Distribution):
